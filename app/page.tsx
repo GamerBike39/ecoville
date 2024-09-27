4ee53965f964e612ff0c0dd754d0c9090e1b5dd9
GamerBike39 <champidub@gmail.com>
2024-09-27 18:05:27 +0200

    comment no using

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/homepage/hero/hero";
 import { Project } from "@/components/homepage/projects/projects";
 import { History } from "@/components/homepage/history/history";
 import { Archi } from "@/components/homepage/architecture/archi";
-import { Governance } from "@/components/homepage/governance/governance";
+// import { Governance } from "@/components/homepage/governance/governance";
 import { Tourisme } from "@/components/homepage/tourisme/tourisme";
 
 export default function Home() {
